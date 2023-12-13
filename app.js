4d78ced794a71a1cdb1dd50d9b2663574a3d8f3e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-13 11:40:44 +0200

    relativeTime

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 'use strict';
 console.log('app.js application running');
 const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
 
 const now = dayjs();
 console.log(now.format());
 // https://day.js.org/docs/en/display/format
 console.log(now.format('YYYY-MMM-DD, dddd'));
 
-console.log('Hello again');
+const tenDaysAhead = now.add(10, 'Day');
+console.log('tenDaysAhead', tenDaysAhead.format('YYYY-MMM-DD, dddd'));
+
+// kiek dienu iki kaledu
+const tillKaledos = now.to('2023-12-25');
+console.log('tillKaledos ===', tillKaledos);
+
+console.log('Hello again !!!');

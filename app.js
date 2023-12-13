014b8f9933fc6e8e187b53c9dbf752882ef9b480
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-13 11:27:34 +0200

    dayjs + format

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 console.log('app.js application running');
+const dayjs = require('dayjs');
 
-console.log('Hello 111');
+const now = dayjs();
+console.log(now.format());
+// https://day.js.org/docs/en/display/format
+console.log(now.format('YYYY-MMM-DD, dddd'));
 
 console.log('Hello again');
